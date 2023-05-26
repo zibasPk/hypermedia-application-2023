a18232624202e7efb6c2fbf6346686f9de4f2edf
lmanini <lmanini@protonmail.com>
2023-05-26 17:36:31 +0200

    white navbar rady

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -52,7 +52,7 @@ function toggle(toShow: element) {
 </script>
 
 <template>
-  <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+  <nav class="bg-white border-black border-solid border-b">
     <div
       class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
     >
@@ -63,14 +63,14 @@ function toggle(toShow: element) {
           alt="Flowbite Logo"
         />
         <span
-          class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+          class="self-center text-2xl font-semibold whitespace-nowrap "
           >Flowbite</span
         >
       </a>
       <button
         data-collapse-toggle="navbar-dropdown"
         type="button"
-        class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200   "
         aria-controls="navbar-dropdown"
         aria-expanded="false"
       >
@@ -91,13 +91,13 @@ function toggle(toShow: element) {
       </button>
       <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
         <ul
-          class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+          class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white  md: "
         >
           <li v-for="element in elements">
             <a
               v-if="element.dropdownElements.length == 0"
               :href="element.link"
-              class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
+              class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:  md:"
               aria-current="page"
             >
               {{ element.text }}
@@ -105,7 +105,7 @@ function toggle(toShow: element) {
             <button
               v-if="element.dropdownElements.length != 0"
               @click="toggle(element)"
-              class="flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+              class="flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto  md:    md:"
             >
               {{ element.text }}
               <svg
@@ -124,13 +124,13 @@ function toggle(toShow: element) {
             </button>
             <div
               v-if="element.dropdownElements.length > 0 && element.toggled.value"
-              class="absolute z-10 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
+              class="absolute z-10 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44  "
             >
-              <ul class="py-2 text-sm text-gray-700 dark:text-gray-400">
+              <ul class="py-2 text-sm text-gray-700 ">
                 <li v-for="dropdownElement in element.dropdownElements">
                   <a
                     :href="dropdownElement.link"
-                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    class="block px-4 py-2 hover:bg-gray-100  "
                     >{{ dropdownElement.text }}</a
                   >
                 </li>
