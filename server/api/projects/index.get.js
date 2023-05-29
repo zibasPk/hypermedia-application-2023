9c4f041677d5a4b01981ad6dbf1993c5ee9511d1
zibas <milo.brontesi@gmail.com>
2023-05-29 12:05:37 +0200

    Added endpoint for most relevant projects

---
+++ server/api/projects/index.get.js
@@ -0,0 +1,15 @@
+import { serverSupabaseClient } from "#supabase/server";
+
+export default defineEventHandler(async (event) => {
+  const client = serverSupabaseClient(event);
+
+  const { data, error } = await client
+    .from("projects")
+    .select("project_code, name, description,relevance, supervisor (member_code,name,surname)")
+    .neq("relevance", 0);
+    
+  if (error) {
+    throw createError({ statusCode: 400, statusMessage: error.message });
+  }
+  return data;
+});
