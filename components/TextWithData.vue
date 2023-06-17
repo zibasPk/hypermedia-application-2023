1a7d69f29cf4dad8d55f161fb7ba3819eee8596e
zibas <milo.brontesi@gmail.com>
2023-06-17 15:12:40 +0200

    Fixed textWithData style

--- components/TextWithData.vue
+++ components/TextWithData.vue
@@ -14,14 +14,14 @@ const props = defineProps({
 
 <template>
   <div class="grid grid-flow-row">
-    <div class="flex" v-for="(item, index) in stringObjectArray">
+    <div class="flex font-bold items-center mt-1" v-for="(item, index) in stringObjectArray">
       <template v-if="index % 2 === 0">
-        <div class="text-secondary text-4xl">{{ item.string1 }}</div>
-        <div class="text-xl">{{ item.string2 }}</div>
+        <div class="text-secondary text-[2.75rem] w-fit">{{ item.string1 }}</div>
+        <div class="text-xl pl-1">{{ item.string2 }}</div>
       </template>
       <template v-else>
         <div class="text-xl">{{ item.string1 }}</div>
-        <div class="text-secondary text-4xl">{{ item.string2 }}</div>
+        <div class="text-secondary text-[2.75rem] pl-1">{{ item.string2 }}</div>
       </template>
     </div>
   </div>
