ac46c3416baa63a91f43f440b637341fce52bf76
zibas <milo.brontesi@gmail.com>
2023-06-21 18:31:24 +0200

    created contact page

---
+++ components/TextField.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps(["value", "classes"]);
+</script>
+
+<template>
+  <textarea
+    class="bg-transparent text-secondarytext border-b border-secondary w-full text-sm align-text-top overflow-hidden"
+    :class="classes"
+  >
+    {{ props.value }}
+  </textarea>
+</template>
