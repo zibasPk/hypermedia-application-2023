6f8ad1332de80aee5b9a6c02bf4d77e7de2b08bd
lmanini <lmanini@protonmail.com>
2023-06-22 18:02:24 +0200

    initial single area page

--- components/TitleTextItem.vue
+++ components/TitleTextItem.vue
@@ -37,7 +37,7 @@ const props = defineProps({
   additionalTitleClasses: {
     type: String,
     required: false,
-    default: "title-primary",
+    default: "text-primary",
   },
 });
 
---
+++ pages/areas/[id].vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+const backgroundImageHeader = {
+  src: "https://dqtgyrjqxnduyldbwyfx.supabase.co/storage/v1/object/public/images/WF%20Hero.jpg",
+  alt: "hero",
+};
+</script>
+
+<template>
+  <PageHeader :image="backgroundImageHeader">
+    <TitleTextItem
+      title="Area Name #1"
+      text="Gabbibbo"
+      centered
+      additionalTextClasses="text-secondarytext"
+      additionalTitleClasses="text-secondarytext"
+    >
+    </TitleTextItem>
+  </PageHeader>
+
+  <!-- Shit about area -->
+  <StandardSlotted>
+    <template v-slot:first>
+      <TitleTextItem
+        title="Sustainable development Goals"
+        text="Testo"
+        additionalTextClasses="mx-auto"
+        divCentered
+      ></TitleTextItem>
+    </template>
+    <template v-slot:second>
+      <div class="overflow-hidden h-[1006px]">
+        <img
+          class="rounded m-auot object-cover h-full"
+          :src="backgroundImageHeader.src"
+          :alt="backgroundImageHeader.alt"
+        />
+      </div>
+    </template>
+  </StandardSlotted>
+
+  <!-- Project related to this area -->
+  <StandardSlotted>
+    <template v-slot:first>
+      <TitleTextItem
+        title="Project of Area 0"
+        text="Testo"
+        additionalTitleClasses="text-primary"
+        additionalTextClasses="mx-auto"
+        divCentered
+      ></TitleTextItem>
+    </template>
+    <template v-slot:second> </template>
+  </StandardSlotted>
+</template>
