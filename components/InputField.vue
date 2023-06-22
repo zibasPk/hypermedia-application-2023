980faf6b95409831211a854dc32ccb390824c27a
zibas <milo.brontesi@gmail.com>
2023-06-22 12:15:03 +0200

    finished contact page

--- components/InputField.vue
+++ components/InputField.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
 const props = defineProps({
+  type: { type: String, required: false, default: "text" },
   value: { type: String, required: false, default: "" },
   classes: { type: String, required: false, default: "" },
 });
 </script>
 
 <template>
-  <textarea
+  <input
+    :type="type"
     class="bg-transparent border-b border-secondary w-full text-sm outline-none focus:border-gray-400 text-secondarytext"
     :classes="classes"
     :placeholder="value"
     required
-  ></textarea>
+  />
 </template>
--- pages/contact.vue
+++ pages/contact.vue
@@ -2,21 +2,21 @@
 <template>
   <StandardSlotted class="pt-10">
     <template v-slot:first>
-      <div class="flex flex-col gap-10 max-w-sm float-left">
-        <h1 class="font-bold text-4xl text-primary pb-10">
+      <div class="flex flex-col gap-10 float-left min-w-[15rem]">
+        <h1 class="font-bold text-4xl text-primary pb-10 max-w-sm">
           Let's discuss something cool together
         </h1>
-        <div class="pl-3">
+        <div class="pl-3 max-w-sm">
           <i class="fa-solid fa-envelope text-secondary pr-3"></i
           >info@DCventure.com
         </div>
         <div
-          class="rounded-lg bg-gray-400 border-gray-500 border-2 text-secondarytext p-3"
+          class="rounded-lg bg-gray-400 border-gray-500 border-2 text-secondarytext p-3 max-w-sm"
         >
           <i class="fa-solid fa-phone pr-3"></i>+123 456 789
         </div>
 
-        <div class="pl-3">
+        <div class="pl-3 max-w-md">
           <i class="fa-solid fa-location-dot pr-3 text-secondary"></i>22nd
           street Seaboard, North Carolina(NC), 27876
         </div>
@@ -24,26 +24,28 @@
     </template>
 
     <template v-slot:second>
-      <div
-        class="flex flex-col gap-10 bg-primary rounded-3xl min-w-[14rem] w-2/3 p-8"
-      >
-        <div class="text-center text-secondarytext font-medium">
-          Send us a message!
-        </div>
-        <div class="flex flex-col gap-10 text-sm">
-          <TextField value="Your name" classes="resize-none w-full" />
-          <TextField value="Your email" classes="resize-none w-full" />
-          <TextField value="Your message" classes="w-full" />
-        </div>
-        <div class="flex w-full pt-20">
-          <FilledButton>
-            <template v-slot:content>
-              <div>
-                <i class="fa-solid fa-paper-plane pr-2"></i>
-                Send Message
-              </div>
-            </template>
-          </FilledButton>
+      <div class="flex justify-end">
+        <div
+          class="flex flex-col gap-10 bg-primary rounded-3xl min-w-[14rem] w-2/3 p-8"
+        >
+          <div class="text-center text-secondarytext font-medium">
+            Send us a message!
+          </div>
+          <div class="flex flex-col gap-10 text-sm">
+            <InputField value="Your name" />
+            <InputField value="Your email" />
+            <TextField value="Your message" class="h-20 resize-none" />
+          </div>
+          <div class="flex w-full pt-28">
+            <FilledButton link="#">
+              <template v-slot:content>
+                <div>
+                  <i class="fa-solid fa-paper-plane pr-2"></i>
+                  Send Message
+                </div>
+              </template>
+            </FilledButton>
+          </div>
         </div>
       </div>
     </template>
