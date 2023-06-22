408f8465b9492d1ac1ef811e78bfb069b1cc90f2
zibas <milo.brontesi@gmail.com>
2023-06-22 17:26:43 +0200

    Button now use NuxtLink

--- components/FilledButton.vue
+++ components/FilledButton.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts">
 const props = defineProps(["link", "classes"]);
-
-function forward() {
-  location.href = props.link;
-}
 </script>
 
 <template>
-  <button
-    class="border-2 text-white bg-secondary border-secondary font-medium rounded-lg text-sm px-5 py-2.5"
-    :class="classes"
-    @click="forward()"
-  >
-    <slot name="content"> </slot>
-  </button>
+  <NuxtLink :to="link" aria-current="page">
+    <button
+      class="border-2 text-white bg-secondary border-secondary font-medium rounded-lg text-sm px-5 py-2.5"
+      :class="classes"
+    >
+      <slot name="content"> </slot>
+    </button>
+  </NuxtLink>
 </template>
--- components/OutlineButton.vue
+++ components/OutlineButton.vue
@@ -2,18 +2,15 @@
 // <OutlineButton content="button" link="https://google.com"/>
 
 const props = defineProps(["link", "classes"]);
-
-function forward() {
-  location.href = props.link;
-}
 </script>
 
 <template>
-  <button
-    class="box-border border-2 border-secondary text-secondary bg-transparent font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
-    :class="classes"
-    @click="forward()"
-  >
-    <slot name="content" />
-  </button>
+  <NuxtLink :to="link" aria-current="page">
+    <button
+      class="box-border border-2 border-secondary text-secondary bg-transparent font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+      :class="classes"
+    >
+      <slot name="content" />
+    </button>
+  </NuxtLink>
 </template>
