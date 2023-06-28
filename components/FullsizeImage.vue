d7b14d5b25790eaf5b5f1b678633e289dbead161
Eliaxie <elia.maggioni@gmail.com>
2023-06-28 16:56:13 +0200

    areas_fixes

--- components/FullsizeImage.vue
+++ components/FullsizeImage.vue
@@ -11,11 +11,11 @@ const props = defineProps({
     :class="
       big
         ? 'md:h-full md:min-h-[600px] h-[600px]'
-        : 'md:h-full md:min-h-[600px] h-[200px]'
+        : 'md:h-full md:min-h-[600px] h-[200px] w-4/5 md:w-auto m-auto'
     "
   >
     <img
-      class="rounded m-auto object-cover h-full"
+      class="rounded m-auto object-cover h-full w-full md:w-auto w-full"
       :src="img.src ?? ''"
       :alt="img.alt ?? ''"
     />
