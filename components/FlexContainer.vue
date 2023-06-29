5130fd8325e95c7729c5a240a70f385bb25af104
zibas <milo.brontesi@gmail.com>
2023-06-29 15:12:24 +0200

    added slug to with projects api

---
+++ components/FlexContainer.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ContentItem } from "~/utils/Types";
+
+const props = defineProps<{
+  content: ContentItem[] | undefined;
+}>();
+</script>
+
+<template>
+  <div class="flex flex-row max-w-full flex-wrap gap-4">
+    <ImageGridItem
+      v-for="item in content"
+      :buttontext="item.buttontext"
+      :buttonlink="item.buttonlink"
+      :maintext="item.maintext"
+      :maindesc="item.maindesc"
+      :rendermaindesc="item.rendermaindesc"
+      :image="item.image"
+      variant="secondary"
+    />
+  </div>
+</template>
