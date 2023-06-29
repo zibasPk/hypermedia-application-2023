0f19fc8911848bf2ba2d60cd9b5f104853e14b19
Eliaxie <elia.maggioni@gmail.com>
2023-06-29 19:55:27 +0200

    seo

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,7 @@
 -->
 <script setup>
 useHead({
+  title: "LVenture Group",
   link: [
     {
       rel: "stylesheet",
@@ -16,8 +17,14 @@ useHead({
   ],
   meta: [
     { charset: "utf-8" },
-    { equiv: "Content-Type", content: "text/html" },
+    { equiv: "Content-Type", content: "text/html; charset=utf-8" },
     { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { name: "title", content: "LVenture Group" },
+    { name: "description", content: "Investing in Change" },
+    { name: "keywords", content: "VC, Investing, in, Change" },
+    { name: "robots", content: "index, follow" },
+    { name: "language", content: "English" },
+    { name: "author", content: "AJBR" },
   ],
 });
 </script>
